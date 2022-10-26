43164d6ef0c303eee39b29d7f9893905e29d0332
Furkan394 <furkanncakiroglu@gmail.com>
2022-10-26 12:58:36 +0300

    camp day 4 / spring boot demo

---
+++ src/main/java/kodlama/io/rentACar/entities/concretes/Brand.java
@@ -0,0 +1,14 @@
+package kodlama.io.rentACar.entities.concretes;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Brand {
+    private int id;
+    private String name;
+
+}
